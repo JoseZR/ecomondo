f819db33e913daf09da9eff21ce1965b5232815a
JoseZR <josezerrod@hotmail.com>
2023-10-15 12:40:19 -0600

    se agrega seccion de Por qué exponer

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -66,84 +66,107 @@ export function Home () {
                             <p>{t('home.text_1')}</p>
                             <p>{t('home.text_2')}</p>
                         </Col>
-                        <Col md={4} className='m-0'>
-                            <img src={i18n.language === 'es' ? '/eco_2024_es.webp' : '/eco_2024_en.webp'} alt="ecomondo" width={300} />
+                        <Col md={4} className=''>
+                            <img src={i18n.language === 'es' ? '/eco_2024_es.webp' : '/eco_2024_en.webp'} alt="ecomondo" width={300} className='w-100'/>
                         </Col>
                     </Row>
                 </article>
             </Container>
             <Container fluid>
-                <p className='fw-bold fs-3 pb-4 home-text-color text-uppercase' style={{ color: '#288705' }}>Ejes del evento</p>
+                <p className='fw-bold fs-3 pb-4 home-text-color text-uppercase' style={{ color: '#288705' }}>{t('home.zone')}</p>
                 <div className='section-evento'>
                     <Row>
-                        <Col xs={12} md={6} lg={3} className='text-center'>
+                        <Col xs={12} md={6} lg={3} className='text-center py-3'>
                             <div className="card-container">
                                 <div className="card">
                                     <div className="front-content">
-                                        <p>Hover me</p>
+                                        <div className="circle circle-1"></div><br />
+                                        <p className='title fs-5 circle-1'>{t('home.zone_1.title')}</p>
                                     </div>
                                     <div className="content">
-                                        <p className="heading">Card Hover</p>
-                                        <p>
-                                            Lorem ipsum dolor sit amet, consectetur adipii
-                                            voluptas ten mollitia pariatur odit, ab
-                                            minus ratione adipisci accusamus vel est excepturi laboriosam magnam
-                                            necessitatibus dignissimos molestias.
-                                        </p>
+                                        <p className="heading">{t('home.zone_1.title')}</p>
+                                        <ul className='text-start'>
+                                            <li>{t('home.zone_1.item_1')}</li>
+                                            <li>{t('home.zone_1.item_2')}</li>
+                                            <li>{t('home.zone_1.item_3')}</li>
+                                            <li>{t('home.zone_1.item_4')}</li>
+                                            <li>{t('home.zone_1.item_5')}</li>
+                                            <li>{t('home.zone_1.item_6')}</li>
+                                            <li>{t('home.zone_1.item_7')}</li>
+                                            <li>{t('home.zone_1.item_8')}</li>
+                                        </ul>
                                     </div>
                                 </div>
                             </div>
                         </Col>
-                        <Col xs={12} md={6} lg={3} className='text-center'>
+                        <Col xs={12} md={6} lg={3} className='text-center py-3'>
                             <div className="card-container">
                                 <div className="card">
-                                    <div className="front-content">
-                                        <p>Hover me</p>
+                                <div className="front-content">
+                                        <div className="circle circle-2"></div><br />
+                                        <p className='title fs-5 circle-2'>{t('home.zone_2.title')}</p>
                                     </div>
                                     <div className="content">
-                                        <p className="heading">Card Hover</p>
-                                        <p>
-                                            Lorem ipsum dolor sit amet, consectetur adipii
-                                            voluptas ten mollitia pariatur odit, ab
-                                            minus ratione adipisci accusamus vel est excepturi laboriosam magnam
-                                            necessitatibus dignissimos molestias.
-                                        </p>
+                                        <p className="heading">{t('home.zone_2.title')}</p>
+                                        <ul className='text-start'>
+                                            <li>{t('home.zone_2.item_1')}</li>
+                                            <li>{t('home.zone_2.item_2')}</li>
+                                            <li>{t('home.zone_2.item_3')}</li>
+                                            <li>{t('home.zone_2.item_4')}</li>
+                                            <li>{t('home.zone_2.item_5')}</li>
+                                            <li>{t('home.zone_2.item_6')}</li>
+                                            <li>{t('home.zone_2.item_7')}</li>
+                                            <li>{t('home.zone_2.item_8')}</li>
+                                        </ul>
                                     </div>
                                 </div>
                             </div>
                         </Col>
-                        <Col xs={12} md={6} lg={3} className='text-center'>
+                        <Col xs={12} md={6} lg={3} className='text-center py-3'>
                             <div className="card-container">
                                 <div className="card">
-                                    <div className="front-content">
-                                        <p>Hover me</p>
+                                <div className="front-content">
+                                    <div className="circle circle-3"></div><br />
+                                        <p className='title fs-5 circle-3'>{t('home.zone_3.title')}</p>
                                     </div>
                                     <div className="content">
-                                        <p className="heading">Card Hover</p>
-                                        <p>
-                                            Lorem ipsum dolor sit amet, consectetur adipii
-                                            voluptas ten mollitia pariatur odit, ab
-                                            minus ratione adipisci accusamus vel est excepturi laboriosam magnam
-                                            necessitatibus dignissimos molestias.
-                                        </p>
+                                    <p className="heading">{t('home.zone_3.title')}</p>
+                                        <ul className='text-start'>
+                                            <li>{t('home.zone_3.item_1')}</li>
+                                            <li>{t('home.zone_3.item_2')}</li>
+                                            <li>{t('home.zone_3.item_3')}</li>
+                                            <li>{t('home.zone_3.item_4')}</li>
+                                            <li>{t('home.zone_3.item_5')}</li>
+                                            <li>{t('home.zone_3.item_6')}</li>
+                                            <li>{t('home.zone_3.item_7')}</li>
+                                        </ul>
                                     </div>
                                 </div>
                             </div>
                         </Col>
-                        <Col xs={12} md={6} lg={3} className='text-center'>
+                        <Col xs={12} md={6} lg={3} className='text-center py-3'>
                             <div className="card-container">
                                 <div className="card">
                                     <div className="front-content">
-                                        <p>Hover me</p>
+                                        <div className="circle circle-4"></div><br />
+                                        <p className='title fs-5 circle-4'>{t('home.zone_4.title')}</p>
                                     </div>
                                     <div className="content">
-                                        <p className="heading">Card Hover</p>
-                                        <p>
-                                            Lorem ipsum dolor sit amet, consectetur adipii
-                                            voluptas ten mollitia pariatur odit, ab
-                                            minus ratione adipisci accusamus vel est excepturi laboriosam magnam
-                                            necessitatibus dignissimos molestias.
-                                        </p>
+                                    <p className="heading">{t('home.zone_4.title')}</p>
+                                        <ul className='text-start'>
+                                            <li>{t('home.zone_4.item_1')}</li>
+                                            <li>{t('home.zone_4.item_2')}</li>
+                                            <li>{t('home.zone_4.item_3')}</li>
+                                            <li>{t('home.zone_4.item_4')}</li>
+                                            <li>{t('home.zone_4.item_5')}</li>
+                                            <li>{t('home.zone_4.item_6')}</li>
+                                            <li>{t('home.zone_4.item_7')}</li>
+                                            <li>{t('home.zone_4.item_8')}</li>
+                                            <li>{t('home.zone_4.item_9')}</li>
+                                            <li>{t('home.zone_4.item_10')}</li>
+                                            <li>{t('home.zone_4.item_11')}</li>
+                                            <li>{t('home.zone_4.item_12')}</li>
+                                        </ul>
                                     </div>
                                 </div>
                             </div>
@@ -151,6 +174,53 @@ export function Home () {
                     </Row>
                 </div>
             </Container>
+            {/* <section className='mt-5 home-whyexhibit'>
+                <p className='fw-bold fs-2'>
+                    <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' width={80}>
+                        <path strokeLinecap='round' strokeLinejoin='round' d='M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 011.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.56.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.893.149c-.425.07-.765.383-.93.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 01-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.397.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 01-.12-1.45l.527-.737c.25-.35.273-.806.108-1.204-.165-.397-.505-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.107-1.204l-.527-.738a1.125 1.125 0 01.12-1.45l.773-.773a1.125 1.125 0 011.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894z' />
+                        <path strokeLinecap='round' strokeLinejoin='round' d='M15 12a3 3 0 11-6 0 3 3 0 016 0z' />
+                    </svg> {t('home.why_exhibit.title')}
+                </p>
+                <p className='d-flex align-items-center gap-3'>
+                    <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' width={40}>
+                        <path strokeLinecap='round' strokeLinejoin='round' d='M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z' />
+                    </svg> {t('home.why_exhibit.item_1')}
+                </p>
+                <p className='d-flex align-items-center gap-3'>
+                    <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' width={40}>
+                        <path strokeLinecap='round' strokeLinejoin='round' d='M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z' />
+                    </svg> {t('home.why_exhibit.item_2')}
+                </p>
+                <p className='d-flex align-items-center gap-3'>
+                    <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' width={40}>
+                        <path strokeLinecap='round' strokeLinejoin='round' d='M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z' />
+                    </svg> {t('home.why_exhibit.item_3')}
+                </p>
+            </section> */}
+            <section className='mt-5 home-whyexhibit'>
+            <p className='fw-bold fs-2'>
+                    <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' width={80}>
+                        <path strokeLinecap='round' strokeLinejoin='round' d='M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 011.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.56.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.893.149c-.425.07-.765.383-.93.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 01-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.397.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 01-.12-1.45l.527-.737c.25-.35.273-.806.108-1.204-.165-.397-.505-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.107-1.204l-.527-.738a1.125 1.125 0 01.12-1.45l.773-.773a1.125 1.125 0 011.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894z' />
+                        <path strokeLinecap='round' strokeLinejoin='round' d='M15 12a3 3 0 11-6 0 3 3 0 016 0z' />
+                    </svg> {t('home.why_exhibit.title')}
+                </p>
+                <Container>
+                    <Row>
+                        <Col xs={12} md={4} lg={4} className='p-5'>
+                            <img src="/item01.webp" alt="ecomondo" />
+                            <p className='d-flex align-items-center gap-3 pt-4'>{t('home.why_exhibit.item_1')}</p>
+                        </Col>
+                        <Col xs={12} md={4} lg={4} className='p-5'>
+                            <img src="/item02.webp" alt="ecomondo" />
+                            <p className='d-flex align-items-center gap-3 pt-4'>{t('home.why_exhibit.item_2')}</p>
+                        </Col>
+                        <Col xs={12} md={4} lg={4} className='p-5'>
+                            <img src="/item03.webp" alt="ecomondo" />
+                            <p className='d-flex align-items-center gap-3 pt-4'>{t('home.why_exhibit.item_3')}</p>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
         </>
     )
 }
