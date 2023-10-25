e0fe9424ede7b8d6a1485d99e16d295923ac81f2
JoseZR <josezerrod@hotmail.com>
2023-10-25 16:48:56 -0600

    se modifica texto del apartado de branding

--- src/components/Schedule/Schedule.jsx
+++ src/components/Schedule/Schedule.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Col, Container, Nav, Row, Tab, Tabs } from 'react-bootstrap';
+import { Accordion, Col, Container, Nav, Row, Tab, Tabs } from 'react-bootstrap';
 import './Program.css';
 import { useTranslation } from 'react-i18next'
 
@@ -254,8 +254,145 @@ export function Schedule() {
             </Tab>
             <Tab eventKey="item3">
               <Container style={{ color: '#ffff' }}>
-                <div className="container mt-5">
-                  <h2><strong>BRANDING</strong></h2>
+              <Row>
+                  <Col lg={4} >
+                    <div className='menu-hijo'>
+                      <Nav className='menu-item-hijo w-100' variant="tabs" activeKey={subMenuActiveTab} onSelect={(key) => handleSubMenuSelect(key)}>
+                        <Nav.Item className="btn-option">
+                          <Nav.Link eventKey="subItem1">
+                            <h4 className=""><strong>CÓRDON DE GAFETE</strong></h4>
+                          </Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item className="btn-option">
+                          <Nav.Link eventKey="subItem2">
+                            <h4 className=""><strong>LOGOTIPO EN GAFETE</strong></h4>
+                          </Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item className="btn-option">
+                          <Nav.Link eventKey="subItem3">
+                            <h4 className=""><strong>GIVE AWAY</strong></h4>
+                          </Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item className="btn-option">
+                          <Nav.Link eventKey="subItem4">
+                            <h4 className=""><strong>REGISTRO</strong></h4>
+                          </Nav.Link>
+                        </Nav.Item>
+                      </Nav>
+                    </div>
+                  </Col>
+                  <Col>
+                    <div>
+                      <Tabs
+                        defaultActiveKey="subItem1"
+                        activeKey={subMenuActiveTab}
+                        id="sub-menu-tab"
+                        className="mt-3"
+                        onSelect={(key) => handleSubMenuSelect(key)}
+                      >
+                        <Tab eventKey="subItem1">
+                          <Container className='text-start px-5 py-3' style={{ color: '#ffff' }}>
+                            <br /><h2><strong>$6,000 USD + IVA</strong></h2><br />
+                            <p className='text-topic ps-4'>
+                                    {t('participate.branding.topic01.item01')}<br />
+                                      {t('participate.branding.topic01.item02')}<br />
+                                        <strong>{t('participate.branding.topic01.item03')}</strong><br />
+                              </p>
+                          </Container>
+                        </Tab>
+                        <Tab eventKey="subItem2">
+                          <Container className='text-start px-5 py-3' style={{ color: '#ffff' }}>
+                              <br /><h2><strong>$3,000 USD + IVA</strong></h2><br />
+                              <p className='text-topic ps-4'>
+                                    {t('participate.branding.topic02.item01')}<br />
+                                      {t('participate.branding.topic02.item02')}<br />
+                              </p>
+                            </Container>
+                        </Tab>
+                        <Tab eventKey="subItem3">
+                          <Container className='text-start px-5 py-3' style={{ color: '#ffff' }}>
+                              <br /><h2><strong>$2,500 USD + IVA</strong></h2><br />
+                              <p className='text-topic ps-4'>
+                                    {t('participate.branding.topic03.item01')}<br />
+                                      {t('participate.branding.topic03.item02')}<br />
+                                        <strong>{t('participate.branding.topic03.item03')}</strong><br />
+                              </p>
+                            </Container>
+                        </Tab>
+                        <Tab eventKey="subItem4">
+                          <Container className='text-start px-5 py-3' style={{ color: '#ffff' }}>
+                              <br /><h2><strong>$6,000 USD + IVA</strong></h2><br />
+                              <p className='text-topic ps-4'>
+                                    {t('participate.branding.topic04.item01')}<br />
+                                      {t('participate.branding.topic04.item02')}<br />
+                                        {t('participate.branding.topic04.item03')}<br />
+                              </p>
+                            </Container>
+                        </Tab>
+                      </Tabs>
+                    </div>
+                  </Col>
+
+
+                  
+                </Row>
+                <div className='branding-padre mt-3'>
+                  <h2 className='mt-5 py-3'><strong>{t('participate.branding_sitio.title')}</strong></h2>
+                  <div className=''>
+                    <Accordion>
+                      <Accordion.Item eventKey="0">
+                        <Accordion.Header><strong>{t('participate.branding_sitio.ladder.title')}</strong></Accordion.Header>
+                        <Accordion.Body className='container-branding'>
+                          <h2><strong>$4,000 USD+ IVA</strong></h2>
+                          <p>{t('participate.branding_sitio.ladder.item01')}</p>
+                          <p><strong>{t('participate.branding_sitio.ladder.title01')}</strong></p>
+                          <p>{t('participate.branding_sitio.description01')}</p>
+                          <p>{t('participate.branding_sitio.description02')}</p>
+                        </Accordion.Body>
+                      </Accordion.Item>
+                      <Accordion.Item eventKey="1">
+                        <Accordion.Header><strong>{t('participate.branding_sitio.pillars.title')}</strong></Accordion.Header>
+                        <Accordion.Body className='container-branding'>
+                          <h2><strong>$2,500 USD + IVA</strong></h2>
+                          <p>{t('participate.branding_sitio.pillars.item01')}</p>
+                          <p><strong>{t('participate.branding_sitio.pillars.title01')}</strong></p>
+                          <p>{t('participate.branding_sitio.description01')}</p>
+                          <p>{t('participate.branding_sitio.description02')}</p>
+                        </Accordion.Body>
+                      </Accordion.Item>
+                      <Accordion.Item eventKey="2">
+                        <Accordion.Header><strong>{t('participate.branding_sitio.stained_glass.title')}</strong></Accordion.Header>
+                        <Accordion.Body className='container-branding'>
+                          <h2><strong>$2,000 USD + IVA</strong></h2>
+                          <p>{t('participate.branding_sitio.stained_glass.item01')}</p>
+                          <p><strong>{t('participate.branding_sitio.stained_glass.title01')}</strong></p>
+                          <p>{t('participate.branding_sitio.description01')}</p>
+                          <p>{t('participate.branding_sitio.description02')}</p>
+                        </Accordion.Body>
+                      </Accordion.Item>
+                      <Accordion.Item eventKey="3">
+                        <Accordion.Header><strong>{t('participate.branding_sitio.lobby_class.title')}</strong></Accordion.Header>
+                        <Accordion.Body className='container-branding'>
+                          <h2><strong>$2,000 USD + IVA</strong></h2>
+                          <p>{t('participate.branding_sitio.lobby_class.item01')}</p>
+                          <p><strong>{t('participate.branding_sitio.lobby_class.title01')}</strong></p>
+                          <p>{t('participate.branding_sitio.description01')}</p>
+                          <p>{t('participate.branding_sitio.description02')}</p>
+                        </Accordion.Body>
+                      </Accordion.Item>
+                      <Accordion.Item eventKey="4">
+                        <Accordion.Header><strong>{t('participate.branding_sitio.ramp.title')}</strong></Accordion.Header>
+                        <Accordion.Body className='container-branding'>
+                          <h2><strong>$2,000 USD + IVA</strong></h2>
+                          <p>{t('participate.branding_sitio.ramp.item01')}</p>
+                          <p><strong>{t('participate.branding_sitio.ramp.title01')}</strong></p>
+                          <p>{t('participate.branding_sitio.description01')}</p>
+                          <p>{t('participate.branding_sitio.description02')}</p>
+                        </Accordion.Body>
+                      </Accordion.Item>
+                    </Accordion>
+                  </div>
+                  
                 </div>
               </Container>
             </Tab>
