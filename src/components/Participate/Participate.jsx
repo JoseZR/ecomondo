5a79e7ed75f1f9b43b9ffd14343bd1a6653787b9
JoseZR <josezerrod@hotmail.com>
2023-11-01 16:12:39 -0600

    cambio en texto en seccion de participantes

--- src/components/Participate/Participate.jsx
+++ src/components/Participate/Participate.jsx
@@ -29,7 +29,7 @@ export function Participate() {
                     <Col xs={12} md={4} className='h-100 p-3'>
                     <div className='container-plant'>
                             <h4 className='py-2'>{t('participate.equipped')}</h4>
-                            <p>9m² {t('participate.item01')}</p><p>USD 370  / m² + IVA</p>
+                            <p>9m² {t('participate.item01')}</p><p>USD 3,330 + IVA</p>
                                 <Row className="justify-content-md-center">
                                     <Col className='m-3'><img src="./alfombra_gris.webp" alt="" className='icon-price'/><br />{t('participate.item05')}</Col>
                                     <Col className='m-3'><img src="./mamparas_gris.webp" alt="" className='icon-price'/><br />{t('participate.item06')}</Col>
@@ -52,29 +52,26 @@ export function Participate() {
                     <Col xs={12} md={4} className='h-100 p-3'>
                     <div className='container-plant'>
                             <h4 className='py-2'>{t('participate.sustainable')}</h4>
-                            <p>2m² {t('participate.item01')}</p><p>USD 2,200 / m² + IVA</p>
+                            <p>{t('participate.area')}</p><p>USD 1,800 + IVA</p>
                                 <Row className="justify-content-md-center">
                                     <Col className='m-3'><img src="./alfombra_gris.webp" alt="" className='icon-price'/><br />{t('participate.item05')}</Col>
-                                    <Col className='m-3'><img src="./mamparas_gris.webp" alt="" className='icon-price'/><br />{t('participate.item06')}</Col>
+                                    <Col className='m-3'><img src="./rotulo.webp" alt="" className='icon-price'/><br />{t('participate.item11')}</Col>
                                 </Row>
                                 <Row className="justify-content-md-center">
                                     <Col className='m-3'><img src="./rotulo.webp" alt="" className='icon-price'/><br />{t('participate.item11')}</Col>
-                                    <Col className='m-3'><img src="./mobiliarip_gris.webp" alt="" className='icon-price'/><br />{t('participate.item09')}</Col>
-                                </Row>
-                                <Row className="justify-content-md-center">
                                     <Col className='m-3'><img src="./contacto_gris.webp" alt="" className='icon-price'/><br />{t('participate.item10')}</Col>
-                                    <Col className='m-3'><img src="./badge.webp" alt="" className='icon-price'/><br />{t('participate.item12')}</Col>
                                 </Row>
                                 <Row className="justify-content-md-center">
+                                    <Col className='m-3'><img src="./badge.webp" alt="" className='icon-price'/><br />{t('participate.item12')}</Col>
                                     <Col className='m-3'><img src="./phone-book.webp" alt="" className='icon-price'/><br />{t('participate.item03')}</Col>
-                                </Row><br />
+                                </Row>
+                                <br />
                             <button className='btn-price'><a href='mailto:laura.padilla@hfmexico.mx'><span className='text-white'>{t('participate.rate_button')}</span></a></button>
                         </div>
                     </Col>
                 </Row>
             </Container>
             <Schedule />
-            
         </>
     )
 }
