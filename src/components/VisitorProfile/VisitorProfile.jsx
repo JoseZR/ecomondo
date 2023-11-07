e896298776dc32059fba3b2b0bee068dfc5ca8aa
JoseZR <josezerrod@hotmail.com>
2023-11-07 17:45:07 -0600

    se modifica seccion de visitantes

--- src/components/VisitorProfile/VisitorProfile.jsx
+++ src/components/VisitorProfile/VisitorProfile.jsx
@@ -9,12 +9,36 @@ export function VisitorProfile () {
                     <i>Ecomondo México</i>
                     <p className='fw-bold fs-1'>{t('visitorProfile.title')}</p>
                     <Row className="justify-content-md-center mt-5 mb-5 visitor">
-                        <Col sm={4} md={6} lg={6}><div className='visitor-profile visitor1' dangerouslySetInnerHTML={{ __html: t('visitorProfile.description') }} /></Col>
+                        <Col sm={4} md={6} lg={6}>
+                            <div className='visitor-profile visitor1'>
+                                <ul>
+                                    <li>{t('visitorProfile.item_1')}</li>
+                                    <li>{t('visitorProfile.item_2')}</li>
+                                    <li>{t('visitorProfile.item_3')}</li>
+                                    <li>{t('visitorProfile.item_4')}</li>
+                                    <li>{t('visitorProfile.item_5')}</li>
+                                    <li>{t('visitorProfile.item_6')}</li>
+                                    <li>{t('visitorProfile.item_7')}</li>
+                                </ul>
+                            </div>
+                        </Col>
                         <Col sm={4} md={6} lg={6} className=''><div className='bg-visitorProfile' /></Col>
                     </Row>
                     <Row className="justify-content-md-center mt-5 mb-5 visitor">
                         <Col sm={4} md={6} lg={6} className='elemento01'><div className='bg-visitorProfile2' /></Col>
-                        <Col sm={4} md={6} lg={6} className='elemento02'><div className='visitor-profile visitor2' dangerouslySetInnerHTML={{ __html: t('visitorProfile.description2') }} /></Col>
+                        <Col sm={4} md={6} lg={6}>
+                            <div className='visitor-profile visitor2'>
+                                <ul>
+                                    <li>{t('visitorProfile.item_8')}</li>
+                                    <li>{t('visitorProfile.item_9')}</li>
+                                    <li>{t('visitorProfile.item_10')}</li>
+                                    <li>{t('visitorProfile.item_11')}</li>
+                                    <li>{t('visitorProfile.item_12')}</li>
+                                    <li>{t('visitorProfile.item_13')}</li>
+                                    <li>{t('visitorProfile.item_14')}</li>
+                                </ul>
+                            </div>
+                        </Col>
                     </Row>
                 </Container>
             </section>
