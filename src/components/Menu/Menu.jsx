089f0cc21f6b0f4927f3e403726c44dcea0cf79a
JoseZR <josezerrod@hotmail.com>
2023-11-09 14:19:42 -0600

    se actualiza seccion  de visitante

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -6,9 +6,8 @@ import './Menu.css'
 import { Container, Nav, NavDropdown, Navbar, Offcanvas } from 'react-bootstrap'
 import { LanguageButton } from './LanguageButton'
 
-
-export function Menu() {
-  const { t, i18n } = useTranslation();
+export function Menu () {
+  const { t, i18n } = useTranslation()
   const [show, setShow] = useState()
 
   const handleShow = () => setShow(true)
@@ -16,65 +15,72 @@ export function Menu() {
 
   return (
     <>
-    <Navbar className='text-uppercase text-center menu-padre'  expand='lg'>
-      <Container>
-        <Navbar.Brand as={Link} to='/'>
-          <img src={logoEco} alt="ecomondo" width="200" className='ms-3' />
-        </Navbar.Brand>
-        <Navbar.Toggle onClick={handleShow}/>
-        <Navbar.Offcanvas 
-          id='offcanvasMenuweb'
-          aria-labelledby='offcanvasMenuweb'
-          placement="end" 
-          show={show}
+      <Navbar className='text-uppercase text-center menu-padre' expand='lg'>
+        <Container>
+          <Navbar.Brand as={Link} to='/'>
+            <img src={logoEco} alt='ecomondo' width='200' className='ms-3' />
+          </Navbar.Brand>
+          <Navbar.Toggle onClick={handleShow} />
+          <Navbar.Offcanvas
+            id='offcanvasMenuweb'
+            aria-labelledby='offcanvasMenuweb'
+            placement='end'
+            show={show}
           >
-        <Offcanvas.Header closeButton onClick={handleClose} />
-        <Offcanvas.Body className='ms-auto'>
-          <Nav className='align-items-center'>
-            <NavDropdown title={t('menu.about_us')} renderMenuOnMount disabled>
-              <NavDropdown.Item as={Link} to='/about-us' onClick={handleClose}>{t('menu.about_1')}</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to='https://hfmexico.mx/about-igeco' target='_blank' onClick={handleClose}>{t('menu.about_2')}</NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title={t('menu.exhibitors')} renderMenuOnMount disabled>
-              <NavDropdown.Item as={Link} to='/contact' onClick={handleClose}>{t('menu.exhibitors_1')}</NavDropdown.Item>
-              <NavDropdown.Item href={i18n.language === 'es'
-                    ? '/ecomondo/files/Factsheet_Ecomondo_Mexico_2024_ES_v061123.pdf'
-                    : '/ecomondo/files/Factsheet-Ecomondo-Mexico-2024_EN-1.pdf'} target='_blank' onClick={handleClose}>{t('menu.exhibitors_2')}
-              </NavDropdown.Item>
-              <NavDropdown.Item  href={'/ecomondo/files/ECO_Y_SSM_2024_V5_11012023_floor_v061123.pdf'} target='_blank' onClick={handleClose}>{t('menu.exhibitors_3')}</NavDropdown.Item>
-              <NavDropdown.Item href={'/ecomondo/files/ASSpaceAgreement-Ecomondo-Mexico.pdf'} target='_blank'  onClick={handleClose}>{t('menu.exhibitors_4')}
-              </NavDropdown.Item>
-              <NavDropdown.Item href={'/ecomondo/files/Contrato-Patrocinios-ECO-1n.pdf'} target='_blank' onClick={handleClose}>{t('menu.exhibitors_5')}</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to='/participate' onClick={handleClose}>{t('menu.exhibitors_6')}</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to='/toolkit' onClick={handleClose}>{t('menu.exhibitors_7')}</NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title={t('menu.visitors')} renderMenuOnMount disabled>
-              <NavDropdown.Item as={Link} to='/visitor-profile' onClick={handleClose}>{t('menu.visitors_1')}</NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title={t('menu.past_events')} renderMenuOnMount disabled>
-              <p className='mt-2 p-2 mb-0'>{t('menu.ecomondo2023')}</p>
-                <NavDropdown.Item href={'/ecomondo/files/Directorio_ECOMONDO_2023.pdf'} target='_blank' onClick={handleClose}>{t('menu.ecomondo2023_1')}</NavDropdown.Item>
-                <NavDropdown.Item href={i18n.language === 'es'
-                    ? '/ecomondo/files/REPORTE-DE-RESULTADOS-ECO-ESP.pdf'
-                    : '/ecomondo/files/AFTERSHOW-REPORT-ECO-ENG.pdf'} target='_blank' onClick={handleClose}>{t('menu.ecomondo2023_2')}</NavDropdown.Item>
-                {/* <NavDropdown.Item as={Link} to='/about-us' onClick={handleClose}>{t('menu.ecomondo2023_3')}</NavDropdown.Item> */}
+            <Offcanvas.Header closeButton onClick={handleClose} />
+            <Offcanvas.Body className='ms-auto'>
+              <Nav className='align-items-center'>
+                <NavDropdown title={t('menu.about_us')} renderMenuOnMount disabled>
+                  <NavDropdown.Item as={Link} to='/about-us' onClick={handleClose}>{t('menu.about_1')}</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to='https://hfmexico.mx/about-igeco' target='_blank' onClick={handleClose}>{t('menu.about_2')}</NavDropdown.Item>
+                </NavDropdown>
+                <NavDropdown title={t('menu.exhibitors')} renderMenuOnMount disabled>
+                  <NavDropdown.Item as={Link} to='/contact' onClick={handleClose}>{t('menu.exhibitors_1')}</NavDropdown.Item>
+                  <NavDropdown.Item
+                    href={i18n.language === 'es'
+                      ? '/ecomondo/files/Factsheet_Ecomondo_Mexico_2024_ES_v061123.pdf'
+                      : '/ecomondo/files/Factsheet-Ecomondo-Mexico-2024_EN-1.pdf'} target='_blank' onClick={handleClose}
+                  >{t('menu.exhibitors_2')}
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href='/ecomondo/files/ECO_Y_SSM_2024_V5_11012023_floor_v061123.pdf' target='_blank' onClick={handleClose}>{t('menu.exhibitors_3')}</NavDropdown.Item>
+                  <NavDropdown.Item href='/ecomondo/files/ASSpaceAgreement-Ecomondo-Mexico.pdf' target='_blank' onClick={handleClose}>{t('menu.exhibitors_4')}
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href='/ecomondo/files/Contrato-Patrocinios-ECO-1n.pdf' target='_blank' onClick={handleClose}>{t('menu.exhibitors_5')}</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to='/participate' onClick={handleClose}>{t('menu.exhibitors_6')}</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to='/toolkit' onClick={handleClose}>{t('menu.exhibitors_7')}</NavDropdown.Item>
+                </NavDropdown>
+                <NavDropdown title={t('menu.visitors')} renderMenuOnMount disabled>
+                  <NavDropdown.Item as={Link} to='/visitor-profile' onClick={handleClose}>{t('menu.visitors_1')}</NavDropdown.Item>
+                </NavDropdown>
+                <NavDropdown title={t('menu.past_events')} renderMenuOnMount disabled>
+                  <p className='mt-2 p-2 mb-0'>{t('menu.ecomondo2023')}</p>
+                  <NavDropdown.Item href='/ecomondo/files/Directorio_ECOMONDO_2023.pdf' target='_blank' onClick={handleClose}>{t('menu.ecomondo2023_1')}</NavDropdown.Item>
+                  <NavDropdown.Item
+                    href={i18n.language === 'es'
+                      ? '/ecomondo/files/REPORTE-DE-RESULTADOS-ECO-ESP.pdf'
+                      : '/ecomondo/files/AFTERSHOW-REPORT-ECO-ENG.pdf'} target='_blank' onClick={handleClose}
+                  >{t('menu.ecomondo2023_2')}
+                  </NavDropdown.Item>
+                  {/* <NavDropdown.Item as={Link} to='/about-us' onClick={handleClose}>{t('menu.ecomondo2023_3')}</NavDropdown.Item> */}
                   <NavDropdown.Divider />
-              <p className='mt-2 p-2 mb-0'>{t('menu.ecomondo2022')}</p>
-                <NavDropdown.Item href={'/ecomondo/files/Directorio_ECOMONDO_2022-1.pdf'} target='_blank' onClick={handleClose}>{t('menu.ecomondo2022_1')}</NavDropdown.Item>
-                <NavDropdown.Item href={i18n.language === 'es'
-                    ? '/ecomondo/files/Resultados_ECO_2022_ES.pdf'
-                    : '/ecomondo/files/Resultados_ECO_2022_EN.pdf'} target='_blank' onClick={handleClose}>{t('menu.ecomondo2022_2')}</NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title={t('menu.media_hub')} renderMenuOnMount disabled>
-                <NavDropdown.Item as={Link} to='/gallery' onClick={handleClose}>{t('menu.media_hub_1')}</NavDropdown.Item>
-            </NavDropdown>
-              <LanguageButton />
-          </Nav> 
-        </Offcanvas.Body>
-        </Navbar.Offcanvas>
-      </Container>
-    </Navbar>
+                  <p className='mt-2 p-2 mb-0'>{t('menu.ecomondo2022')}</p>
+                  <NavDropdown.Item href='/ecomondo/files/Directorio_ECOMONDO_2022-1.pdf' target='_blank' onClick={handleClose}>{t('menu.ecomondo2022_1')}</NavDropdown.Item>
+                  <NavDropdown.Item
+                    href={i18n.language === 'es'
+                      ? '/ecomondo/files/Resultados_ECO_2022_ES.pdf'
+                      : '/ecomondo/files/Resultados_ECO_2022_EN.pdf'} target='_blank' onClick={handleClose}
+                  >{t('menu.ecomondo2022_2')}
+                  </NavDropdown.Item>
+                </NavDropdown>
+                <NavDropdown title={t('menu.media_hub')} renderMenuOnMount disabled>
+                  <NavDropdown.Item as={Link} to='/gallery' onClick={handleClose}>{t('menu.media_hub_1')}</NavDropdown.Item>
+                </NavDropdown>
+                <LanguageButton />
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
     </>
-    
   )
 }
--- src/components/VisitorProfile/VisitorProfile.jsx
+++ src/components/VisitorProfile/VisitorProfile.jsx
@@ -1,48 +1,137 @@
-import { Col, Container, Row} from 'react-bootstrap'
+import { useEffect } from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
 import './VisitorProfile.css'
-import { t } from 'i18next'
-export function VisitorProfile () {
-    return(
-        <>
-            <section  className='bg-secondary-text'>
-                <Container className='mt-5 mb-5'>
-                    <i>Ecomondo México</i>
-                    <p className='fw-bold fs-1'>{t('visitorProfile.title')}</p>
-                    <Row className="justify-content-md-center mt-5 mb-5 visitor">
-                        <Col sm={4} md={6} lg={6}>
-                            <div className='visitor-profile visitor1'>
-                                <ul>
-                                    <li>{t('visitorProfile.item_1')}</li>
-                                    <li>{t('visitorProfile.item_2')}</li>
-                                    <li>{t('visitorProfile.item_3')}</li>
-                                    <li>{t('visitorProfile.item_4')}</li>
-                                    <li>{t('visitorProfile.item_5')}</li>
-                                    <li>{t('visitorProfile.item_6')}</li>
-                                    <li>{t('visitorProfile.item_7')}</li>
-                                </ul>
-                            </div>
-                        </Col>
-                        <Col sm={4} md={6} lg={6} className=''><div className='bg-visitorProfile' /></Col>
-                    </Row>
-                    <Row className="justify-content-md-center mt-5 mb-5 visitor">
-                        <Col sm={4} md={6} lg={6} className='elemento01'><div className='bg-visitorProfile2' /></Col>
-                        <Col sm={4} md={6} lg={6}>
-                            <div className='visitor-profile visitor2'>
-                                <ul>
-                                    <li>{t('visitorProfile.item_8')}</li>
-                                    <li>{t('visitorProfile.item_9')}</li>
-                                    <li>{t('visitorProfile.item_10')}</li>
-                                    <li>{t('visitorProfile.item_11')}</li>
-                                    <li>{t('visitorProfile.item_12')}</li>
-                                    <li>{t('visitorProfile.item_13')}</li>
-                                    <li>{t('visitorProfile.item_14')}</li>
-                                </ul>
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
-            </section>
+import { useTranslation } from 'react-i18next'
+import Aos from 'aos'
+import 'aos/dist/aos.css' // Importa los estilos CSS de AOS
 
-        </>
-    )
+export function VisitorProfile () {
+  const { t } = useTranslation()
+  useEffect(() => {
+    Aos.init()
+  }, [])
+  return (
+    <>
+      <div className='exhibitor-back'>
+        <div className='content-visitor'>
+          <div className='pt-5' style={{ color: '#fff' }}>
+            <i>Ecomondo México</i>
+            <p className='fw-bold fs-1'>{t('visitorProfile.title')}</p>
+          </div>
+          <Container className='w-100'>
+            <Row>
+              <Col className='py-3'>
+
+                <div
+                  data-aos='fade-right'
+                  data-aos-offset='300'
+                  data-aos-easing='ease-in-sine'
+                  data-aos-duration='1000'
+                >
+                  <ul className='lista-visitor'>
+                    <li className='item-res'>
+                      <svg className='item-icon-visitor' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor'>
+                        <path stroke-linecap='round' stroke-linejoin='round' d='M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z' />
+                      </svg>
+                      <span className='item-text-visitor'>{t('visitorProfile.item_1')}</span>
+                    </li>
+                    <li className='item-res'>
+                      <svg className='item-icon-visitor' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor'>
+                        <path stroke-linecap='round' stroke-linejoin='round' d='M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z' />
+                      </svg>
+                      <span className='item-text-visitor'>{t('visitorProfile.item_2')}</span>
+                    </li>
+                    <li className='item-res'>
+                      <svg className='item-icon-visitor' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor'>
+                        <path stroke-linecap='round' stroke-linejoin='round' d='M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z' />
+                      </svg>
+                      <span className='item-text-visitor'>{t('visitorProfile.item_3')}</span>
+                    </li>
+                    <li className='item-res'>
+                      <svg className='item-icon-visitor' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor'>
+                        <path stroke-linecap='round' stroke-linejoin='round' d='M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z' />
+                      </svg>
+                      <span className='item-text-visitor'>{t('visitorProfile.item_4')}</span>
+                    </li>
+                    <li className='item-res'>
+                      <svg className='item-icon-visitor' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor'>
+                        <path stroke-linecap='round' stroke-linejoin='round' d='M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z' />
+                      </svg>
+                      <span className='item-text-visitor'>{t('visitorProfile.item_5')}</span>
+                    </li>
+                    <li className='item-res'>
+                      <svg className='item-icon-visitor' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor'>
+                        <path stroke-linecap='round' stroke-linejoin='round' d='M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z' />
+                      </svg>
+                      <span className='item-text-visitor'>{t('visitorProfile.item_6')}</span>
+                    </li>
+                    <li className='item-res'>
+                      <svg className='item-icon-visitor' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor'>
+                        <path stroke-linecap='round' stroke-linejoin='round' d='M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z' />
+                      </svg>
+                      <span className='item-text-visitor'>{t('visitorProfile.item_7')}</span>
+                    </li>
+                  </ul>
+                </div>
+              </Col>
+              <Col className='py-3 '>
+                <div
+                  data-aos='fade-right'
+                  data-aos-offset='300'
+                  data-aos-easing='ease-in-sine'
+                  data-aos-duration='1000'
+                >
+                  <ul className='lista-visitor'>
+                    <li className='item-res'>
+                      <svg className='item-icon-visitor' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor'>
+                        <path stroke-linecap='round' stroke-linejoin='round' d='M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z' />
+                      </svg>
+                      <span className='item-text-visitor'>{t('visitorProfile.item_8')}</span>
+                    </li>
+                    <li className='item-res'>
+                      <svg className='item-icon-visitor' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor'>
+                        <path stroke-linecap='round' stroke-linejoin='round' d='M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z' />
+                      </svg>
+                      <span className='item-text-visitor'>{t('visitorProfile.item_9')}</span>
+                    </li>
+                    <li className='item-res'>
+                      <svg className='item-icon-visitor' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor'>
+                        <path stroke-linecap='round' stroke-linejoin='round' d='M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z' />
+                      </svg>
+                      <span className='item-text-visitor'>{t('visitorProfile.item_10')}</span>
+                    </li>
+                    <li className='item-res'>
+                      <svg className='item-icon-visitor' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor'>
+                        <path stroke-linecap='round' stroke-linejoin='round' d='M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z' />
+                      </svg>
+                      <span className='item-text-visitor'>{t('visitorProfile.item_11')}</span>
+                    </li>
+                    <li className='item-res'>
+                      <svg className='item-icon-visitor' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor'>
+                        <path stroke-linecap='round' stroke-linejoin='round' d='M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z' />
+                      </svg>
+                      <span className='item-text-visitor'>{t('visitorProfile.item_12')}</span>
+                    </li>
+                    <li className='item-res'>
+                      <svg className='item-icon-visitor' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor'>
+                        <path stroke-linecap='round' stroke-linejoin='round' d='M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z' />
+                      </svg>
+                      <span className='item-text-visitor'>{t('visitorProfile.item_13')}</span>
+                    </li>
+                    <li className='item-res'>
+                      <svg className='item-icon-visitor' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor'>
+                        <path stroke-linecap='round' stroke-linejoin='round' d='M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z' />
+                      </svg>
+                      <span className='item-text-visitor'>{t('visitorProfile.item_14')}</span>
+                    </li>
+                  </ul>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </div>
+
+    </>
+  )
 }
