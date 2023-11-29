762619554b59eb582949d41e68124ebfd17e1dc9
JoseZR <josezerrod@hotmail.com>
2023-11-29 12:12:32 -0600

    se actualiza Fasctsheet ecomondo 2024 por narrativa

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -38,8 +38,8 @@ export function Menu () {
                   <NavDropdown.Item as={Link} to='/contact' onClick={handleClose}>{t('menu.exhibitors_1')}</NavDropdown.Item>
                   <NavDropdown.Item
                     href={i18n.language === 'es'
-                      ? '/ecomondo/files/Factsheet_Ecomondo_Mexico_2024_ESP_v291123.pdf'
-                      : '/ecomondo/files/Factsheet_Ecomondo_Mexico_2024_ENG_v291123.pdf'} target='_blank' onClick={handleClose}
+                      ? '/ecomondo/files/Factsheet_Ecomondo_Mexico_2024_ESP_v291123_1.pdf'
+                      : '/ecomondo/files/Factsheet_Ecomondo_Mexico_2024_ENG_v291123_1.pdf'} target='_blank' onClick={handleClose}
                   >{t('menu.exhibitors_2')}
                   </NavDropdown.Item>
                   <NavDropdown.Item href='/ecomondo/files/ECOMONDO-Y-SSM-PLANO-2024-v5.pdf' target='_blank' onClick={handleClose}>{t('menu.exhibitors_3')}</NavDropdown.Item>
