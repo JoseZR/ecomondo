e1416c1cc78f3dcab8bdfc1743db42e06960576d
JoseZR <josezerrod@hotmail.com>
2023-12-06 13:44:26 -0600

    se actualiza plano Eco 2024

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -28,7 +28,7 @@ export function Home () {
                 <path strokeLinecap='round' strokeLinejoin='round' d='M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25M9 16.5v.75m3-3v3M15 12v5.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z' />
               </svg>{t('home.factsheet')}
             </a>
-            <a href='/ecomondo/files/ECOMONDO-Y-SSM-PLANO-2024-v5_061223.pdf' target='_blank' rel='noreferrer' className='sub-menu-doc'>
+            <a href='/ecomondo/files/ECOMONDO-Y-SSM-PLANO-2024-v6_061223.pdf' target='_blank' rel='noreferrer' className='sub-menu-doc'>
               <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' width={30}>
                 <path strokeLinecap='round' strokeLinejoin='round' d='M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 01-1.125-1.125v-3.75zM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-8.25zM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-2.25z' />
               </svg> {t('home.floorplan')}
