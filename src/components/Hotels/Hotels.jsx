97d65e604821adcbc2b889784f1ca6a3b288e530
JoseZR <josezerrod@hotmail.com>
2023-12-11 10:59:26 -0600

    se actualizo texto y se modifico tamano de boton

--- src/components/Hotels/Hotels.jsx
+++ src/components/Hotels/Hotels.jsx
@@ -7,24 +7,24 @@ export function Hotels () {
   const { t } = useTranslation()
   return (
     <>
-      <div className='mt-5 container'>
+      <div className='mt-5 mb-5 container'>
         <h1>{t('home.hotels')}</h1>
         <p dangerouslySetInnerHTML={{ __html: t('hotels.description') }} />
         <div className='mt-5 hotels'>
           {hotels.map((hotel, index) => {
             return (
-              <Card style={{ width: '18rem' }} key={index}>
+              <Card style={{ width: '18rem', border: '1px solid #ddd' }} key={index}>
                 <Card.Img variant='top' src={hotel.src} height={280} className='object-fit-cover' />
                 <Card.Body style={{ flex: '0' }}>
-                  <Card.Title>{hotel.name}</Card.Title>
-                  <Card.Text>
+                  <Card.Title className='text-description'>{hotel.name}</Card.Title>
+                  <Card.Text className='text-description'>
                     {hotel.address}<br />
                     <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' width={20}>
                       <path strokeLinecap='round' strokeLinejoin='round' d='M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3' />
                     </svg> {hotel.phone}
                   </Card.Text>
                 </Card.Body>
-                <ListGroup className='list-group-flush'>
+                <ListGroup className='list-group-flush text-description'>
                   <ListGroup.Item>{t('hotels.rate')}{hotel.rate} + {t('hotels.taxes')} </ListGroup.Item>
                   <ListGroup.Item>{hotel.email}</ListGroup.Item>
                   <ListGroup.Item>{t('hotels.code')} <strong>{hotel.code}</strong></ListGroup.Item>
@@ -32,8 +32,8 @@ export function Hotels () {
                   <ListGroup.Item><img src='/ecomondo/car_icon.webp' width={20} /> {hotel.time_drive}</ListGroup.Item>
                 </ListGroup>
                 {hotel?.link &&
-                  <Card.Body className='text-center d-grid' style={{ placeContent: 'center' }}>
-                    <Button href={hotel.link} target='_blank'>{t('hotels.book')}</Button>
+                  <Card.Body className='text-center' style={{ placeContent: 'center' }}>
+                    <Button className='btn-reserve' href={hotel.link} target='_blank'>{t('hotels.book')}</Button>
                   </Card.Body>}
               </Card>
             )
