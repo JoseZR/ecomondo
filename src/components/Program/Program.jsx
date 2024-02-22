95992f50d37ff1611782ff0aee00605eab1ffd4f
Jesus Zermeño <josezerrod@hotmail.com>
2024-02-22 03:27:09 -0600

    se agrega fotos de ponentes

--- src/components/Program/Program.jsx
+++ src/components/Program/Program.jsx
@@ -3,11 +3,21 @@ import "./Program.css";
 import { Accordion, Container, Nav, Tab, Tabs } from "react-bootstrap";
 
 import { programECO } from "../../constans_program";
+import { DetailSpeaker } from './DetailSpeaker'
+import { speakers } from '../../constans_speakers'
 import { useState } from "react";
 
 export function Program() {
   const { i18n, t } = useTranslation();
   const [programs, setPrograms] = useState(programECO?.stage_1);
+
+  const [modalShow, setModalShow] = useState(false)
+  const [detailSpeaker, setDetailSpeaker] = useState({})
+
+  const handleModal2 = (speaker) => {
+    setDetailSpeaker(speaker)
+    setModalShow(true)
+  }
   console.log(programs);
   return (
     <>
@@ -93,10 +103,10 @@ export function Program() {
                           <p className="fw-semibold fst-italic text-start"> {program?.speakers} </p>
                           <p>
                             <p className='text-justify'>
-                                  {i18n.language === 'en'
-                                    ? program?.resume_en
-                                    : program?.resume}
-                                  </p> 
+                              {i18n.language === 'en'
+                                ? program?.resume_en
+                                : program?.resume}
+                            </p>
                           </p>
                           <p className="text-start pt-2">
                             <span className="bg-dark text-light fw-bold rounded-pill px-4 py-2">
@@ -242,6 +252,27 @@ export function Program() {
               </Tab>
             </Tabs>
           </div>
+        </Container>
+        <Container>
+            <h1 className='mt-5 text-light text-start' id='ponentes'> {t("program.title2")} </h1>
+            <div className='mt-5 pb-5 program-wrapper-speakers'>
+              {speakers.map((speaker, index) => (
+                <button key={index} onClick={() => handleModal2(speaker)}>
+                  <div className='speaker-item h-100 rounded'>
+                    <img src={speaker.avatar} width={300} height={300} alt={speaker.name} />
+                    <div className='info_speakers'>
+                      <p className="fs-4 fw-bolder">{speaker.name}</p>
+                    </div>
+                  </div>
+                </button>
+              ))}
+              <div />
+            </div>
+            <DetailSpeaker
+              show={modalShow}
+              onHide={() => setModalShow(false)}
+              speaker={detailSpeaker}
+            />
         </Container>
       </div>
     </>
