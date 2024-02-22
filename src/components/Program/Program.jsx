bffc01b4da1e512821a934b446f7cc9bf14e9c60
Jesus Zermeño <josezerrod@hotmail.com>
2024-02-21 23:53:22 -0600

    se cambian agregan nombres de ponentes

--- src/components/Program/Program.jsx
+++ src/components/Program/Program.jsx
@@ -1,239 +1,243 @@
-import { useTranslation } from 'react-i18next'
-import './Program.css'
-import { Container, Nav, Tab, Tabs } from 'react-bootstrap'
+import { useTranslation } from "react-i18next";
+import "./Program.css";
+import { Accordion, Container, Nav, Tab, Tabs } from "react-bootstrap";
 
-import { programECO } from '../../constans_program'
-import { useState } from 'react'
+import { programECO } from "../../constans_program";
+import { useState } from "react";
 
 export function Program() {
-  const { i18n, t } = useTranslation()
-  const [programs, setPrograms] = useState(programECO?.stage_1)
-  console.log(programs)
+  const { i18n, t } = useTranslation();
+  const [programs, setPrograms] = useState(programECO?.stage_1);
+  console.log(programs);
   return (
     <>
-      <div className='program-wrapper program-dates'>
-        <Container className='pt-5 pb-5'>
-          <h1 className='text-light fw-font text-start'>
-            {t('program.title')}
+      <div className="program-wrapper program-dates">
+        <Container className="pt-5 pb-5">
+          <h1 className="text-light fw-font text-start">
+            {t("program.title")}
           </h1>
-          <p className='text-start text-light fs-4'>
-            {t('program.description')}
+          <p className="text-start text-light fs-4">
+            {t("program.description")}
           </p>
-          <Nav className='pt-5' variant='tabs' defaultActiveKey='#stage_1'>
+          <Nav className="pt-5" variant="tabs" defaultActiveKey="#stage_1">
             <Nav.Item>
               <Nav.Link
-                href='#stage_1'
+                href="#stage_1"
                 onClick={() => setPrograms(programECO.stage_1)}
               >
-                <h4 className='text-light'>
-                  {t('home.zone_1.title')} | {t('home.zone_2.title')}
+                <h4 className="text-light">
+                  {t("home.zone_1.title")} | {t("home.zone_2.title")}
                 </h4>
               </Nav.Link>
             </Nav.Item>
             <Nav.Item>
               <Nav.Link
-                href='#stage_2'
+                href="#stage_2"
                 onClick={() => setPrograms(programECO.stage_2)}
               >
-                <h4 className='text-light'>{t('home.zone_3.title')}</h4>
+                <h4 className="text-light">{t("home.zone_3.title")}</h4>
               </Nav.Link>
             </Nav.Item>
           </Nav>
-
-          <div className=' mt-5'>
+          <div className=" mt-5">
             <Tabs
-              defaultActiveKey='day_1'
-              id='fill-tab-example'
-              className='mt-5'
-              style={{ backgroundColor: '#618264' }}
+              defaultActiveKey="day_1"
+              id="fill-tab-example"
+              className="mt-5"
+              style={{ backgroundColor: "#618264" }}
             >
               <Tab
-                eventKey='day_1'
+                eventKey="day_1"
                 title={
                   <>
                     {/* <strong className='fs-6 fw-bold'>
                       {t('program.day')} 1
                     </strong> */}
                     <br />
-                    <span className='fs-4 fw-medium'>{t('program.day_1')}</span>
+                    <span className="fs-4 fw-medium">{t("program.day_1")}</span>
                   </>
                 }
-                style={{ backgroundColor: '#456e46' }}
+                style={{ backgroundColor: "#456e46" }}
               >
                 {programs?.april_17?.map((program, index) => {
                   return (
-                    <div key={index} className='program-date-item'>
-                      <div style={{ width: '85px' }}>
+                    <div key={index} className="program-date-item">
+                      <div style={{ width: "85px" }}>
                         <p>
                           <strong>{program.hour}</strong>
                           <br />
                           <svg
                             width={20}
-                            xmlns='http://www.w3.org/2000/svg'
-                            fill='none'
-                            viewBox='0 0 24 24'
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
                             strokeWidth={1.5}
-                            stroke='currentColor'
+                            stroke="currentColor"
                           >
                             <path
-                              strokeLinecap='round'
-                              strokeLinejoin='round'
-                              d='M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z'
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
                             />
-                          </svg>{' '}
+                          </svg>{" "}
                           {program.duration}
                         </p>
                       </div>
-                      <div className='d-md-flex align-items-center justify-content-between w-100'>
-                        <div className='w-100 pe-4'>
-                          <p className='fs-4 fw-bold text-start'>
-                            {i18n.language === 'en'
+                      <div className="d-md-flex align-items-center justify-content-between w-100">
+                        <div className="w-100 pe-4 text-start">
+                          <p className="fs-3 fw-bold text-start">
+                            {i18n.language === "en"
                               ? program?.title_en
                               : program?.title}
                           </p>
-                          <p className='text-start'>
-                            {i18n.language === 'en'
-                              ? program?.resume_en
-                              : program?.resume}
+                          <p className="fw-semibold fst-italic text-start"> {program?.speakers} </p>
+                          <p>
+                            <p className='text-justify'>
+                                  {i18n.language === 'en'
+                                    ? program?.resume_en
+                                    : program?.resume}
+                                  </p> 
                           </p>
-                          <p className='text-start'>
-                            <span className='bg-dark text-light fw-bold rounded-pill p-2'>
-                              {i18n.language === 'en'
+                          <p className="text-start pt-2">
+                            <span className="bg-dark text-light fw-bold rounded-pill px-4 py-2">
+                              {i18n.language === "en"
                                 ? program.type_en
                                 : program.type}
                             </span>
                           </p>
                         </div>
-                        <div className='d-block text-md-end' />
+                        <div className="d-block text-md-end" />
                       </div>
                     </div>
-                  )
+                  );
                 })}
               </Tab>
               <Tab
-                eventKey='day_2'
+                eventKey="day_2"
                 title={
                   <>
                     {/* <strong className='fs-6 fw-bold'>
                       {t('program.day')} 2
                     </strong> */}
                     <br />
-                    <span className='fs-4 fw-medium'>{t('program.day_2')}</span>
+                    <span className="fs-4 fw-medium">{t("program.day_2")}</span>
                   </>
                 }
-                style={{ backgroundColor: '#456e46' }}
+                style={{ backgroundColor: "#456e46" }}
               >
                 {programs?.april_18?.map((program, index) => {
                   return (
-                    <div key={index} className='program-date-item'>
-                      <div style={{ width: '85px' }}>
+                    <div key={index} className="program-date-item">
+                      <div style={{ width: "85px" }}>
                         <p>
                           <strong>{program.hour}</strong>
                           <br />
                           <svg
                             width={20}
-                            xmlns='http://www.w3.org/2000/svg'
-                            fill='none'
-                            viewBox='0 0 24 24'
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
                             strokeWidth={1.5}
-                            stroke='currentColor'
+                            stroke="currentColor"
                           >
                             <path
-                              strokeLinecap='round'
-                              strokeLinejoin='round'
-                              d='M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z'
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
                             />
-                          </svg>{' '}
+                          </svg>{" "}
                           {program.duration}
                         </p>
                       </div>
-                      <div className='d-md-flex align-items-center justify-content-between w-100'>
-                        <div className='w-100 pe-4'>
-                          <p className='fs-4 fw-bold text-start'>
-                            {i18n.language === 'en'
+                      <div className="d-md-flex align-items-center justify-content-between w-100">
+                        <div className="w-100 pe-4">
+                          <p className="fs-4 fw-bold text-start">
+                            {i18n.language === "en"
                               ? program?.title_en
                               : program?.title}
                           </p>
-                          <p className='text-start'>
-                            {i18n.language === 'en'
+                          <p className="fw-semibold fst-italic text-start"> {program?.speakers} </p>
+                          <p className="text-start">
+                            {i18n.language === "en"
                               ? program?.resume_en
                               : program?.resume}
                           </p>
-                          <p className='text-start'>
-                            <span className='bg-dark text-light fw-bold rounded-pill p-2'>
-                              {i18n.language === 'en'
+                          <p className="text-start pt-2">
+                            <span className="bg-dark text-light fw-bold rounded-pill px-4 py-2">
+                              {i18n.language === "en"
                                 ? program.type_en
                                 : program.type}
                             </span>
                           </p>
                         </div>
-                        <div className='d-block text-md-end' />
+                        <div className="d-block text-md-end" />
                       </div>
                     </div>
-                  )
+                  );
                 })}
               </Tab>
               <Tab
-                eventKey='day_3'
+                eventKey="day_3"
                 title={
                   <>
                     {/* <strong className='fs-6 fw-bold'>
                       {t('program.day')} 3
                     </strong> */}
                     <br />
-                    <span className='fs-4 fw-medium'>{t('program.day_3')}</span>
+                    <span className="fs-4 fw-medium">{t("program.day_3")}</span>
                   </>
                 }
-                style={{ backgroundColor: '#456e46' }}
+                style={{ backgroundColor: "#456e46" }}
               >
                 {programs?.april_19?.map((program, index) => {
                   return (
-                    <div key={index} className='program-date-item'>
-                      <div style={{ width: '85px' }}>
+                    <div key={index} className="program-date-item">
+                      <div style={{ width: "85px" }}>
                         <p>
                           <strong>{program.hour}</strong>
                           <br />
                           <svg
                             width={20}
-                            xmlns='http://www.w3.org/2000/svg'
-                            fill='none'
-                            viewBox='0 0 24 24'
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
                             strokeWidth={1.5}
-                            stroke='currentColor'
+                            stroke="currentColor"
                           >
                             <path
-                              strokeLinecap='round'
-                              strokeLinejoin='round'
-                              d='M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z'
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
                             />
-                          </svg>{' '}
+                          </svg>{" "}
                           {program.duration}
                         </p>
                       </div>
-                      <div className='d-md-flex align-items-center justify-content-between w-100'>
-                        <div className='w-100 pe-4'>
-                          <p className='fs-4 fw-bold text-start'>
-                            {i18n.language === 'en'
+                      <div className="d-md-flex align-items-center justify-content-between w-100">
+                        <div className="w-100 pe-4">
+                          <p className="fs-4 fw-bold text-start">
+                            {i18n.language === "en"
                               ? program?.title_en
                               : program?.title}
                           </p>
-                          <p className='text-start'>
-                            {i18n.language === 'en'
+                          <p className="fw-semibold fst-italic text-start"> {program?.speakers} </p>
+                          <p className="text-start">
+                            {i18n.language === "en"
                               ? program?.resume_en
                               : program?.resume}
                           </p>
-                          <p className='text-start'>
-                            <span className='bg-dark text-light fw-bold rounded-pill p-2'>
-                              {i18n.language === 'en'
+                          <p className="text-start py-2">
+                            <span className="bg-dark text-light fw-bold rounded-pill px-4 py-2">
+                              {i18n.language === "en"
                                 ? program.type_en
                                 : program.type}
                             </span>
                           </p>
                         </div>
-                        <div className='d-block text-md-end' />
+                        <div className="d-block text-md-end" />
                       </div>
                     </div>
-                  )
+                  );
                 })}
               </Tab>
             </Tabs>
@@ -241,5 +245,5 @@ export function Program() {
         </Container>
       </div>
     </>
-  )
+  );
 }
--- src/constans_program.js
+++ src/constans_program.js
@@ -10,24 +10,26 @@ const programECO = {
         duration: '3 hrs.',
         hour: '11:00 - 14:00 hrs.',        
         resume: 'Se centrarán en diversos enfoques para evitar que los residuos orgánicos generen y emitan biogás de vertedero. Las técnicas abordadas son: compostaje, digestión anaerobia y recogida de gas de vertedero. El seminario también estudiará el aspecto financiero en relación con la necesidad de instituciones internacionales disponibles para apoyar la inversión en estas nuevas tecnologías sostenibles.',
-        resume_en: 'The event will focus on various approaches to prevent organic waste to generate and emit biogas from landfills. The techniques addressed are: composting, anaerobic digestion and landfill gas collection. The seminar will also address the financial aspect regarding the need for international institutions available to support the investment into these new, sustainable technologies.',        
-        speakers: [
-          {
-            id: 1,
-          },
-          {
-            id: 2,
-          },
-          {
-            id: 3,
-          },
-          {
-            id: 4,
-          },
-          {
-            id: 5,
-          }
-        ],
+        resume_en: 'The event will focus on various approaches to prevent organic waste to generate and emit biogas from landfills. The techniques addressed are: composting, anaerobic digestion and landfill gas collection. The seminar will also address the financial aspect regarding the need for international institutions available to support the investment into these new, sustainable technologies.',   
+        speakers : 'Jorge Israel García Ochoa | Izarelli | Rosillo | Marco Ricci | Paula García | R3VER. Linares',
+        speakers_en : ""
+        // speakers: [
+        //   {
+        //     id: 1,
+        //   },
+        //   {
+        //     id: 2,
+        //   },
+        //   {
+        //     id: 3,
+        //   },
+        //   {
+        //     id: 4,
+        //   },
+        //   {
+        //     id: 5,
+        //   }
+        // ],
       },
       {        
         type: 'Seminario',
@@ -39,11 +41,7 @@ const programECO = {
         hour: '15:00 - 17:00 hrs.',
         resume: 'Las industrias pueden utilizar soluciones de economía circular para mejorar la sustentabilidad y la productividad. El taller presentará casos de estudio e innovaciones de plantas productivas en México de los sectores: Bioenergía, Alimentos y Bebidas, Automotriz y más.',
         resume_en: 'Industries may use circular economy solutions to improve sustainability and productivity. The workshop will present case studies and innovations of productive plants in Mexico of the sectors: Bio Energy, Food and Beverage, Automotive and more.',
-        speakers: [
-          {
-            id: 6,
-          },          
-        ],
+        speakers : 'Cecile De Mauleon',
       }      
     ],
     april_18: [
@@ -57,17 +55,7 @@ const programECO = {
         hour: '11:00 a 13:00 hrs.',
         resume: 'En el evento se presentará la Guía del Profesional centrada en la cuestión de los contaminantes en el reciclado de residuos orgánicos, un tema que aborda también otros flujos de reciclado como envases, RAEE, etc. El documento será presentado por uno de los autores principales y los ponentes se encuentran entre los colaboradores de la Guía o expertos de América Latina y de México. Organizado/ promovido en cooperación con la Red Regional ISWA para América Latina coordinada por ARS-Argentina.',
         resume_en: 'The event will present the Practitioner’s Guide focusing on the issue of contaminant in organic waste recycling, a topic which addresses also other recycling streams such as packaging, WEEE, etc. The document will be presented by one of the lead-authors and the speakers are among the contributors of the Guide or experts from Latin-American and from Mexico.  Organised/promoted in cooperation with the ISWA Regional Network for Latin America coordinated by ARS-Argentina.',
-        speakers: [
-          {
-            id: 10,
-          },
-          {
-            id: 11,
-          },
-          {
-            id: 3,
-          }
-        ],
+        speakers : 'Francisco Galván | Marco Ricci | Gustavo Solórzano',
       },
       {
         type: 'Seminario',
@@ -79,11 +67,7 @@ const programECO = {
         hour: '15:30 a 17:00 hrs.',
         resume: 'Ciudades verdes y desarrollo sostenible en América Latina: nuevas fronteras de la Economía Circular',
         resume_en: 'Green cities and sustainable development in Latin America: new frontiers of the circular economy',
-        speakers: [
-          {
-            id: 13,
-          },          
-        ],
+        speakers : 'Giselle Canahuati'
       },
     ],
     april_19: [
@@ -97,8 +81,7 @@ const programECO = {
         hour: '11:00 a 12:00 hrs.',
         resume: 'Con la llegada de la movilidad eléctrica y la presión para la descarbonización del sistema de transporte, se están generando cuellos de botella para diferentes residuos, ante la premura de sustituir los vehículos, generando componentes útiles (autopartes)',
         resume_en: 'With the arrival of electric mobility and the pressure for the decarbonization of the transportation system, bottlenecks are being generated for different wastes, since there is pressure to replace vehicles faster, generating useful components (auto parts)',
-        speakers: [          
-        ],
+        speakers : '' 
       },
       {
         type: 'Seminario',
@@ -110,8 +93,7 @@ const programECO = {
         hour: '12:15 a 13:15 hrs.',
         resume: 'Se discutirán las legislaciones y procedimientos para el manejo de estos residuos, casos de éxito en Centroamérica, la necesidad de promover en México la recolección y clasificación adecuada, así como el aprovechamiento de la oportunidad comercial.',
         resume_en: 'Legislation and procedures for the management of this waste, success stories in Central America, the need to promote in Mexico the collection and proper classification, exploitation of the commercial opportunity.',
-        speakers: [          
-        ],
+        speakers : '' 
       },
     ],   
   },
@@ -127,8 +109,7 @@ const programECO = {
         hour: '11:00 a 14:00 hrs.',
         resume:'Las innovaciones cercanas al mercado integradas en la infraestructura hídrica existente o nueva son el desafío al que la transferencia de conocimientos y tecnología debe hacer frente. El taller presentará innovaciones relevantes que han alcanzado un alto nivel de preparación tecnológica (TRL), incluyendo criterios de diseño y estrategias para integrarlos en estudios de casos reales.',
         resume_en:'Close to market innovations integrated in the existing or new water infrastructure. This is the challenge that knowledge and technology transfer should address. The workshop will present relevant innovations that have reached high technology readiness level (TRL) including design criteria and strategies to integrate them in real case studies',
-        speakers: [
-        ],        
+        speakers : ''       
       },
       {
         type: 'Seminario',
@@ -140,17 +121,7 @@ const programECO = {
         hour: '17:00 - 18:30 hrs.',
         resume: 'Esta mesa redonda de comparación Italia/ Europa/ México pondrá a discusión estudios de tratamiento sostenible de aguas industriales así como aguas residuales, y la reutilización de este recurso, con casos de Europa y diferentes estados de México.',
         resume_en: 'International round table comparing: Italy/Europe, Mexico on the topic of sustainable industrial water and wastewater treatment and water reuse. Case studies from Europe and different Mexico states.',
-        speakers: [
-          {
-            id: 7,
-          },
-          {
-            id: 8,
-          },
-          {
-            id: 9,
-          }
-        ],
+        speakers : 'Francesco Fatone | Jesús Manríquez | José Tello'
       }
     ],
     april_18: [
@@ -164,14 +135,21 @@ const programECO = {
         hour: '13:00 a 14:30 hrs.',
         resume: 'El uso excesivo o la contaminación de la calidad de las aguas superficiales o subterráneas puede afectar a amplias zonas y requerir mucho tiempo y grandes inversiones para su reparación. Se necesitan políticas y normativas que abarquen toda la cuenca para impulsar planes de acción integrados. En el taller se presentarán estudios de casos mexicanos y se entablará un debate constructivo con expertos europeos.',
         resume_en: 'Overuse or pollution of surface or groundwater quality might impact wide areas and require long time and high investments for remediation. Catchment-wide policies and regulations are needed to drive integrated action plans. The workshop will present Mexican case studied and discussion with European experts will drive the constructive discussion.',
-        speakers: [
-          {
-            id: 12,
-          },
-          {
-            id: 7,
-          },        
-        ],
+        speakers : 'Cindy McCulligh | Francesco Fatone' 
+      }
+    ],
+    april_19: [
+      {
+        type: 'Información por confirmar',
+        type_en:'Information to be completed',
+        title: 'PRÓXIMAMENTE',
+        title_en: '',
+        location: '',
+        duration: '0 hrs.',
+        hour: '00:00 a 00:00 hrs.',
+        resume: '',
+        resume_en: '',
+        speakers : ''
       }
     ],
   }, 
