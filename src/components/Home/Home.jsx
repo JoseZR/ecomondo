ff4eaf15c5e4067d22cc6bdaab2484fe25721c28
Jesus Zermeño <josezerrod@hotmail.com>
2024-02-26 00:02:54 -0600

    se agrega descripción a imagenes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -433,43 +433,43 @@ export function Home() {
             <Row className='text-center fs-6 fw-semibold fst-italic'>
               <Col className='text-center'>
                 <div className='topic-size'>
-                  <img src="/ecomondo/topic_1.webp" alt="" style={{ width: '120px', height: '120px' }} />
+                  <img src="/ecomondo/topic_1.webp" alt="gestión de desperdicios y residuos"  width='100px' loading='lazy'/>
                   <p className='pt-3'>{t("home.interests.item1")}</p>
                 </div>
               </Col>
               <Col>
                 <div className='topic-size'>
-                  <img src="/ecomondo/topic_2.webp" alt="" style={{ width: '120px', height: '120px' }} />
+                  <img src="/ecomondo/topic_2.webp" alt="manejo de residuos químicos" width='100px' loading='lazy'/>
                   <p className='pt-3'>{t("home.interests.item5")}</p>
                 </div>
               </Col>
               <Col>
                 <div className='topic-size'>
-                  <img src="/ecomondo/topic_3.webp" alt="" style={{ width: '120px', height: '120px' }} />
+                  <img src="/ecomondo/topic_3.webp" alt="bioenergia" width='100px' loading='lazy'/>
                   <p className='pt-3'>{t("home.interests.item2")}</p>
                 </div>
               </Col>
               <Col>
                 <div className='topic-size'>
-                  <img src="/ecomondo/topic_4.webp" alt="" style={{ width: '120px', height: '120px' }} />
+                  <img src="/ecomondo/topic_4.webp" alt="iluminación Inteligente" width='100px' loading='lazy'/>
                   <p className='pt-3'>{t("home.interests.item6")}</p>
                 </div>
               </Col>
               <Col>
                 <div className='topic-size'>
-                  <img src="/ecomondo/topic_5.webp" alt="" style={{ width: '120px', height: '120px' }} />
+                  <img src="/ecomondo/topic_5.webp" alt="tratamiento de agua" width='100px' loading='lazy'/>
                   <p className='pt-3'>{t("home.interests.item3")}</p>
                 </div>
               </Col>
               <Col>
                 <div className='topic-size'>
-                  <img src="/ecomondo/topic_6.webp" alt="" style={{ width: '120px', height: '120px' }} />
+                  <img src="/ecomondo/topic_6.webp" alt="certificaciones" width='100px' loading='lazy'/>
                   <p className='pt-3'>{t("home.interests.item7")}</p>
                 </div>
               </Col>
               <Col>
                 <div className='topic-size'>
-                  <img src="/ecomondo/topic_7.webp" alt="" style={{ width: '120px', height: '120px' }} />
+                  <img src="/ecomondo/topic_7.webp" alt="transporte de desechos" width='100px' loading='lazy'/>
                   <p className='pt-3'>{t("home.interests.item4")}</p>
                 </div>
               </Col>
