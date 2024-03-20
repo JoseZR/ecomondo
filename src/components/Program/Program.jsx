7d65a9407b657b34ad65b921b11e2f6ac42657cf
Diegodev2002 <chila_diego@outlook.com>
2024-03-20 12:50:07 -0600

    cambio de tamaño el modal de speakers

--- src/components/Program/Program.jsx
+++ src/components/Program/Program.jsx
@@ -1,4 +1,4 @@
-import { useTranslation } from "react-i18next";
+ import { useTranslation } from "react-i18next";
 import "./Program.css";
 import { Accordion, Col, Container, Nav, Row, Tab, Tabs } from "react-bootstrap";
 
