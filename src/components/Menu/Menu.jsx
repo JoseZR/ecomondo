02da9b543bfc834bdbeee004c8245a72d9963f09
Jesus Zermeño <josezerrod@hotmail.com>
2024-05-17 15:36:02 -0600

    se actualiza plano

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -121,7 +121,7 @@ export function Menu() {
                     {t('menu.sponsorships')}
                   </NavDropdown.Item>
                   <NavDropdown.Item
-                    href='/files/ECOMONDO_floorplan_2025_v7_08_05_2024.pdf'
+                    href='/files/ECOMONDO_floorplan_2025_v7_015_05_2024.pdf.pdf'
                     target='_blank'
                     onClick={handleClose}
                   >
