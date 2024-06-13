85052f53a5c9d04dbda33b3170c32bad3c293f62
Jesus Zermeño <josezerrod@hotmail.com>
2024-06-13 08:46:21 -0600

    se actualiza contrato de eco

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -128,7 +128,7 @@ export function Menu() {
                     {t('menu.exhibitors_3')}
                   </NavDropdown.Item>
                   <NavDropdown.Item
-                    href='/files/ECOMONDO 2025 SpaceAgreement ED.pdf'
+                    href='/files/ECOMONDO_2025_SpaceAgreement_ED_13062424.pdf'
                     target='_blank'
                     onClick={handleClose}
                   >
