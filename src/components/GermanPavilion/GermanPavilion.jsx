cd26c56db90d0fe0b4c5a1077a6621b8281ca196
Jesus Zermeño <josezerrod@hotmail.com>
2024-10-14 16:41:03 -0600

    se agrega información a la sección de pabellón alemán

--- src/components/GermanPavilion/GermanPavilion.jsx
+++ src/components/GermanPavilion/GermanPavilion.jsx
@@ -4,16 +4,16 @@ import './GermanPavilion.css';
 export function GermanPavilion() {
     const { t } = useTranslation()
     return (
-       <>
+        <>
             <div className='bg-pavilion sub-text'>
                 <div>
                     <p className='fw-bold pt-5 title-germanPavilion1'>{t("german-pavilion.title")}</p>
-                    <hr className='liner'/>
+                    <hr className='liner' />
                     <p className='fs-2 fw-bolder'>ECOMONDO</p>
                     <p className=''> {t("german-pavilion.date")}</p><br />
-                    <p  className='text-desc' dangerouslySetInnerHTML={{ __html: t("german-pavilion.description1") }} />
+                    <p className='text-desc' dangerouslySetInnerHTML={{ __html: t("german-pavilion.description1") }} />
                 </div>
-            </div> 
+            </div>
             <div className='bg-prices'>
                 <div>
                     <p className='title-germanPavilion2 fw-bold'>{t("german-pavilion.partnership.title")}</p>
@@ -24,6 +24,42 @@ export function GermanPavilion() {
                     </div>
                 </div>
             </div>
-       </>
+            <div className='pt-5'>
+                <p className='title-germanPavilion3 fw-bold title-germanPavilion2'>{t("german-pavilion.information-center.title")}</p>
+                <div className='packages'>
+                    <div className='text-start'>
+                        <p> ◇ {t("german-pavilion.information-center.item01")}</p>
+                        <p> ◇ {t("german-pavilion.information-center.item02")}</p>
+                        <p> ◇ {t("german-pavilion.information-center.item03")}</p>
+                        <p> ◇ {t("german-pavilion.information-center.item04")}</p>
+                        <p> ◇ {t("german-pavilion.information-center.item05")}</p>
+                        <p> ◇ {t("german-pavilion.information-center.item06")}</p>
+                    </div>
+                    <img src="/centro_informacion.webp" alt="stand information center"  className='size-img-information'/>
+                </div>
+            </div>
+            <div className='pt-5'>
+                <p className='title-germanPavilion3 fw-bold title-germanPavilion2'>{t("german-pavilion.stand-space.title")}</p>
+                <div className='packages'>
+                <img src="/eco-stand.webp" alt="stand information center"  className='size-img-information'/>
+                    <div className='text-start'>
+                        <p> ◇ {t("german-pavilion.stand-space.item01")}</p>
+                        <p> ◇ {t("german-pavilion.stand-space.item02")}</p>
+                        <p> ◇ {t("german-pavilion.stand-space.item03")}</p>
+                        <p> ◇ {t("german-pavilion.stand-space.item04")}</p>
+                        <p> ◇ {t("german-pavilion.stand-space.item05")}</p>
+                        <p> ◇ {t("german-pavilion.stand-space.item06")}</p>
+                        <p> ◇ {t("german-pavilion.stand-space.item07")}</p>
+                        <p> ◇ {t("german-pavilion.stand-space.item08")}</p>
+                        <p> ◇ {t("german-pavilion.stand-space.item09")}</p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <p className='title-germanPavilion2' dangerouslySetInnerHTML={{ __html: t("german-pavilion.participation-fee.element1") }} />
+                <p className='title-germanPavilion2' dangerouslySetInnerHTML={{ __html: t("german-pavilion.participation-fee.element2") }} />
+                <p className='title-deadline' dangerouslySetInnerHTML={{ __html: t("german-pavilion.participation-fee.deadline") }} />
+            </div>
+        </>
     )
 }
